3356160c65b8baa3b8a6431b61ece129819e6eae
kevin <kevinmalaverr@gmail.com>
2020-02-01 21:43:07 -0500

    subida

--- js/transition.js
+++ js/transition.js
@@ -5,3 +5,15 @@ function change(url){
         document.location.href = url;
     }, 440); 
 }
+
+$("a[href*=\\#]").click(function() {
+    if (location.pathname.replace(/^\//,"") == this.pathname.replace(/^\//,"")
+    && location.hostname == this.hostname) {
+    var $target = $(this.hash);
+    $target = $target.length && $target || $("[name=" + this.hash.slice(1) +"]");
+    if ($target.length) {
+    var targetOffset = $target.offset().top;
+    $("html,body").animate({scrollTop: targetOffset}, 1000);
+    }
+    }
+});
